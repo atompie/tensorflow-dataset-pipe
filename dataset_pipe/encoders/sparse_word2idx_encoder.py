4e4908e44f80dfb9f3aa68b8e00a46980a6e79de
risto <risto.kowaczewski@gmail.com>
2021-03-05 00:55:08 +0100

    encoders

--- dataset_pipe/encoders/sparse_word2idx_encoder.py
+++ dataset_pipe/encoders/sparse_word2idx_encoder.py
@@ -1,13 +1,15 @@
 import numpy as np
+from dataset_pipe.encoders.math.ops import zeros
 
 
 class SparseWord2IdxEncoder:
 
-    def __init__(self, dim, word2idx, no_word_idx=None):
+    def __init__(self, word2idx, width, no_word_idx=None):
         self.no_word_idx = no_word_idx
         self.word2idx = word2idx
-        self._dim = dim
-        self._shape = (dim,)
+        self.width = width
+        self._dim = 1
+        self._shape = (width, 1)
         self._type = "float32"
 
     def encode(self, data):
@@ -15,14 +17,16 @@ class SparseWord2IdxEncoder:
             raise ValueError("Param data must be list. {} given fo type {}".format(data, type(data)))
 
         vector = []
-        for word in data:
+        for word in data[:self.width]:
             if word in self.word2idx:
                 vector.append(self.word2idx[word])
             elif self.no_word_idx:
                 vector.append(self.no_word_idx)
 
         vector = np.array(vector)
-        return np.expand_dims(vector, axis=-1)
+        z = zeros(self.width)
+        z[:vector.shape[0]] = vector
+        return np.expand_dims(z, axis=-1)
 
     def shape(self):
         return self._shape
